bdde900099568068b20d1e0f57ec1cab9f496c33
Matheus Almeida <mat.almeida@live.com>
2019-09-02 23:46:12 -0300

    chore(server): add info log when server start running

--- src/server.js
+++ src/server.js
@@ -3,4 +3,6 @@ import config from './config';
 
 app.listen(config.PORT, () => {
   Object.keys(config).map((key) => console.info(`${key}: ${config[key]}`));
+
+  console.info(`Server running on http://localhost:${config.PORT}`);
 });
